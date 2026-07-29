1dcd0be21c7c8148540c5fac20e5906ec5e371e6
Henrique Abreu <abreuh873@gmail.com>
2026-07-29 10:46:26 -0300

    feat: create the controller and implement the first route

--- src/main/java/dev/henriqueabreu/ToDoList/Tasks/ToDoController.java
+++ src/main/java/dev/henriqueabreu/ToDoList/Tasks/ToDoController.java
@@ -1,4 +1,16 @@
 package dev.henriqueabreu.ToDoList.Tasks;
 
-public class Controller {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/todo")
+public class ToDoController {
+
+    @GetMapping("/sobre")
+    public String sobre() {
+        return "Bem vindo ao seu TODO List, que vai te ajudar a organizar suas tarefas de forma simples e fácil!";
+    }
+
 }
